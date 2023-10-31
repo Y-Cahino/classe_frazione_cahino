fda48d9715671b8a413b00063436213ac7fec38e
Cahinosilva.20184 <Cahinosilva.20184@E2-9-PC03>
2023-10-31 13:11:51 +0100

    Aggiungere i file di progetto.

---
+++ classe_frazione_cahino/Frazione.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classe_frazione_cahino
+{
+    class Frazione
+    {
+        private double a, b,c;
+        class somma:Frazione
+        {
+            public somma()
+            {
+                a= 0;
+                b= 0;
+            }
+            public somma
+        }
+        public void setx(double x)
+        {
+            if (x > 0)
+            {
+                a = x;
+            }
+        }
+        public void sety(double x)
+        {
+            if (x > 0)
+            {
+                a = x;
+            }
+        }
+    }
+}
